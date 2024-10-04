14b537718b486a2645b6bdcf6a8b0d3a12060138
sonawane-sakshi <h116sonawanesakshi@gmail.com>
2024-10-04 09:57:27 +0530

    Add files via upload

---
+++ data tyes.cpp
@@ -0,0 +1,32 @@
+//sakshi Sonawane 
+//Entc A
+//PRN: 24070123505
+//Experiment- 2
+#include <iostream>
+#include <string> // Include for string
+using namespace std;
+int main() {
+    int int_v;
+    cout << "Size of integer is: " << sizeof(int_v) << " bytes" << endl;
+
+    float float_v;
+    cout << "Size of float is: " << sizeof(float_v) << " bytes" << endl;
+
+    double double_v;
+    cout << "Size of double is: " << sizeof(double_v) << " bytes" << endl;
+
+    char char_v;
+    cout << "Size of character is: " << sizeof(char_v) << " bytes" << endl;
+
+    string string_v;
+    cout << "Size of string object is: " << sizeof(string_v) << " bytes" << endl;
+
+    bool bool_v;
+    cout << "Size of boolean is: " << sizeof(bool_v) << " bytes" << endl;
+
+    const int const_int_v = 10;
+    cout << "Size of constant integer is: " << sizeof(const_int_v) << " bytes" << endl;
+
+    return 0;
+}
+
